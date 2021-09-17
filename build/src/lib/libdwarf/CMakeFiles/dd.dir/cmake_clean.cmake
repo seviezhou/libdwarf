89b90dccec91050b9c0b965307dc5bd7c98a8e50
seviezhou <seviezhou@163.com>
2021-09-17 10:43:41 +0800

    init aafe188d1b8a2d50c4bdd1d8890980f0f5c65f84

---
+++ build/src/lib/libdwarf/CMakeFiles/dd.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dd"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/dd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/lib/libdwarf/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/seviezhou/Downloads/libdwarf-code-aafe188d1b8a2d50c4bdd1d8890980f0f5c65f84/src/lib/libdwarf
+# Build directory: /Users/seviezhou/Downloads/libdwarf-code-aafe188d1b8a2d50c4bdd1d8890980f0f5c65f84/build/src/lib/libdwarf
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
