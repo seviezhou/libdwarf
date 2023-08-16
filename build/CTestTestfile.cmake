202ec41e988dd069c7c3a5caae7fc24e8b179410
seviezhou <seviezhou@163.com>
2023-08-16 11:28:04 +0800

    remove build

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/seviezhou/Downloads/libdwarf-code-aafe188d1b8a2d50c4bdd1d8890980f0f5c65f84
-# Build directory: /Users/seviezhou/Downloads/libdwarf-code-aafe188d1b8a2d50c4bdd1d8890980f0f5c65f84/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src/lib/libdwarf")
-subdirs("src/bin/dwarfdump")
--- build/src/bin/dwarfdump/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/seviezhou/Downloads/libdwarf-code-aafe188d1b8a2d50c4bdd1d8890980f0f5c65f84/src/bin/dwarfdump
-# Build directory: /Users/seviezhou/Downloads/libdwarf-code-aafe188d1b8a2d50c4bdd1d8890980f0f5c65f84/build/src/bin/dwarfdump
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
